9ede05a07d35c1ab79244edf2d069c62d7ce4760
Ramon Lopes <dev.ramondasilvalopes@gmail.com>
2024-02-12 23:43:53 -0300

    attda faculdade

--- MODULO1A/DES.DINAMICO/JS_com_readline_sync/script2.js
+++
@@ -1,7 +0,0 @@
-
-// trabalhando com js usando o readline-sync par afazer perguntas. obs: ainda não conseguir configurar.
-
-let dados = require('readline-sync');
-
-let nome = dados.question("qual o seu nome")
-
--- MODULO1A/DES.DINAMICO/JS_puro/script1.js
+++
@@ -1,5 +0,0 @@
-// declaração de variaveis normal  usando js puro
-
-let nome = "Ramon"
-
-console.log("ola " + nome)
